a3c1d3136eee7e1dfd0bd93bea9df43fd204ee03
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2026-06-21 12:15:30 -0400

    Open sourced (from private repo) BCL.Throw helper class.

---
+++ NorthSouthSystems.Opinions.BCL.Tests/T_Throw.cs
@@ -0,0 +1,157 @@
+﻿public class T_Throw
+{
+    [Fact]
+    public void GreaterThanEnum()
+    {
+        Throw.IfGreaterThanEnum(SomeEnum.Zero, SomeEnum.Zero);
+        Throw.IfGreaterThanEnum(SomeEnum.Zero, SomeEnum.One);
+        Throw.IfGreaterThanEnum(SomeEnum.One, SomeEnum.Two);
+
+        Action action = () => Throw.IfGreaterThanEnum(SomeEnum.One, SomeEnum.Zero);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfGreaterThanEnum(SomeEnum.Two, SomeEnum.One);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void GreaterThanOrEqualEnum()
+    {
+        Throw.IfGreaterThanOrEqualEnum(SomeEnum.Zero, SomeEnum.One);
+        Throw.IfGreaterThanOrEqualEnum(SomeEnum.One, SomeEnum.Two);
+
+        Action action = () => Throw.IfGreaterThanOrEqualEnum(SomeEnum.Zero, SomeEnum.Zero);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfGreaterThanOrEqualEnum(SomeEnum.One, SomeEnum.Zero);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void LessThanEnum()
+    {
+        Throw.IfLessThanEnum(SomeEnum.Zero, SomeEnum.Zero);
+        Throw.IfLessThanEnum(SomeEnum.One, SomeEnum.Zero);
+        Throw.IfLessThanEnum(SomeEnum.Two, SomeEnum.One);
+
+        Action action = () => Throw.IfLessThanEnum(SomeEnum.Zero, SomeEnum.One);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfLessThanEnum(SomeEnum.One, SomeEnum.Two);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void LessThanOrEqualEnum()
+    {
+        Throw.IfLessThanOrEqualEnum(SomeEnum.One, SomeEnum.Zero);
+        Throw.IfLessThanOrEqualEnum(SomeEnum.Two, SomeEnum.One);
+
+        Action action = () => Throw.IfLessThanOrEqualEnum(SomeEnum.One, SomeEnum.One);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfLessThanOrEqualEnum(SomeEnum.One, SomeEnum.Two);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void Between()
+    {
+        Throw.IfBetween(0, 1, 2);
+        Throw.IfBetween(3, 1, 2);
+        Throw.IfBetween(0, 1, 3);
+        Throw.IfBetween(4, 1, 3);
+
+        Action action = () => Throw.IfBetween(1, 1, 2);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetween(2, 1, 2);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetween(1, 1, 3);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetween(2, 1, 3);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetween(3, 1, 3);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void NotBetween()
+    {
+        Throw.IfNotBetween(1, 1, 2);
+        Throw.IfNotBetween(2, 1, 2);
+        Throw.IfNotBetween(1, 1, 3);
+        Throw.IfNotBetween(2, 1, 3);
+        Throw.IfNotBetween(3, 1, 3);
+
+        Action action = () => Throw.IfNotBetween(0, 1, 2);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfNotBetween(3, 1, 2);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfNotBetween(0, 1, 3);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfNotBetween(4, 1, 3);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void BetweenEnum()
+    {
+        Throw.IfBetweenEnum(SomeEnum.Zero, SomeEnum.One, SomeEnum.Two);
+        Throw.IfBetweenEnum(SomeEnum.Three, SomeEnum.One, SomeEnum.Two);
+        Throw.IfBetweenEnum(SomeEnum.Zero, SomeEnum.One, SomeEnum.Three);
+        Throw.IfBetweenEnum(SomeEnum.Four, SomeEnum.One, SomeEnum.Three);
+
+        Action action = () => Throw.IfBetweenEnum(SomeEnum.One, SomeEnum.One, SomeEnum.Two);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetweenEnum(SomeEnum.Two, SomeEnum.One, SomeEnum.Two);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetweenEnum(SomeEnum.One, SomeEnum.One, SomeEnum.Three);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetweenEnum(SomeEnum.Two, SomeEnum.One, SomeEnum.Three);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfBetweenEnum(SomeEnum.Three, SomeEnum.One, SomeEnum.Three);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void NotBetweenEnum()
+    {
+        Throw.IfNotBetweenEnum(SomeEnum.One, SomeEnum.One, SomeEnum.Two);
+        Throw.IfNotBetweenEnum(SomeEnum.Two, SomeEnum.One, SomeEnum.Two);
+        Throw.IfNotBetweenEnum(SomeEnum.One, SomeEnum.One, SomeEnum.Three);
+        Throw.IfNotBetweenEnum(SomeEnum.Two, SomeEnum.One, SomeEnum.Three);
+        Throw.IfNotBetweenEnum(SomeEnum.Three, SomeEnum.One, SomeEnum.Three);
+
+        Action action = () => Throw.IfNotBetweenEnum(SomeEnum.Zero, SomeEnum.One, SomeEnum.Two);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfNotBetweenEnum(SomeEnum.Three, SomeEnum.One, SomeEnum.Two);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfNotBetweenEnum(SomeEnum.Zero, SomeEnum.One, SomeEnum.Three);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+
+        action = () => Throw.IfNotBetweenEnum(SomeEnum.Four, SomeEnum.One, SomeEnum.Three);
+        action.Should().ThrowExactly<ArgumentOutOfRangeException>();
+    }
+
+    private enum SomeEnum
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4
+    }
+}
---
+++ NorthSouthSystems.Opinions.BCL/Throw.cs
@@ -0,0 +1,253 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace NorthSouthSystems;
+
+#pragma warning disable CA1716 // Accepted as the preferred design.
+public static class Throw
+#pragma warning restore
+{
+    public static T IfNull<T>([NotNull] T? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        where T : class
+    {
+        ArgumentNullException.ThrowIfNull(argument, paramName);
+
+        return argument;
+    }
+
+    public static T IfDefault<T>(T argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        where T : struct
+    {
+        ArgumentExceptionX.ThrowIfDefault(argument, paramName);
+
+        return argument;
+    }
+
+    public static T IfDefault<T>([NotNull] T? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        where T : struct
+    {
+        ArgumentExceptionX.ThrowIfDefault(argument, paramName);
+
+        return argument.Value;
+    }
+
+    #region String (ArgumentException pass-through)
+
+    public static string IfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(argument, paramName);
+
+        return argument;
+    }
+
+    public static string IfNullOrWhiteSpace([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(argument, paramName);
+
+        return argument;
+    }
+
+    #endregion
+
+    #region INumberBase<T> (ArgumentOutOfRangeException pass-through)
+
+    public static T IfZero<T>(T value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : INumberBase<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfZero(value, paramName);
+
+        return value;
+    }
+
+    public static T IfNegative<T>(T value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : INumberBase<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(value, paramName);
+
+        return value;
+    }
+
+    public static T IfNegativeOrZero<T>(T value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : INumberBase<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(value, paramName);
+
+        return value;
+    }
+
+    #endregion
+
+    #region Equality (ArgumentOutOfRangeException pass-through)
+
+    public static T IfEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+    {
+        ArgumentOutOfRangeException.ThrowIfEqual(value, other, paramName);
+
+        return value;
+    }
+
+    public static T IfNotEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+    {
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value, other, paramName);
+
+        return value;
+    }
+
+    #endregion
+
+    #region IComparable<T> (ArgumentOutOfRangeException pass-through)
+
+    public static T IfGreaterThan<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : IComparable<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(value, other, paramName);
+
+        return value;
+    }
+
+    public static T IfGreaterThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : IComparable<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(value, other, paramName);
+
+        return value;
+    }
+
+    public static T IfLessThan<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : IComparable<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(value, other, paramName);
+
+        return value;
+    }
+
+    public static T IfLessThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : IComparable<T>
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(value, other, paramName);
+
+        return value;
+    }
+
+    #endregion
+
+    #region Comparer<TEnum> (custom)
+
+    public static TEnum IfGreaterThanEnum<TEnum>(TEnum value, TEnum other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where TEnum : struct, Enum
+    {
+        if (Comparer<TEnum>.Default.Compare(value, other) > 0)
+            ComparerTEnumThrow(value, other, paramName, "less than or equal to");
+
+        return value;
+    }
+
+    public static TEnum IfGreaterThanOrEqualEnum<TEnum>(TEnum value, TEnum other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where TEnum : struct, Enum
+    {
+        if (Comparer<TEnum>.Default.Compare(value, other) >= 0)
+            ComparerTEnumThrow(value, other, paramName, "less than");
+
+        return value;
+    }
+
+    public static TEnum IfLessThanEnum<TEnum>(TEnum value, TEnum other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where TEnum : struct, Enum
+    {
+        if (Comparer<TEnum>.Default.Compare(value, other) < 0)
+            ComparerTEnumThrow(value, other, paramName, "greater than or equal to");
+
+        return value;
+    }
+
+    public static TEnum IfLessThanOrEqualEnum<TEnum>(TEnum value, TEnum other, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where TEnum : struct, Enum
+    {
+        if (Comparer<TEnum>.Default.Compare(value, other) <= 0)
+            ComparerTEnumThrow(value, other, paramName, "greater than");
+
+        return value;
+    }
+
+    // Extracted in order to make the successful path smaller and more likely to inline.
+    [DoesNotReturn]
+    private static void ComparerTEnumThrow<TEnum>(TEnum value, TEnum other, string? paramName, string inverseOperatorFriendlyName)
+        where TEnum : struct, Enum =>
+        throw new ArgumentOutOfRangeException(paramName, value,
+            string.Create(CultureInfo.InvariantCulture, $"{paramName} ('{value}') must be {inverseOperatorFriendlyName} '{other}'."));
+
+    #endregion
+
+    #region IComparable<T> Between (custom)
+
+    public static T IfBetween<T>(T value, T leftInclusive, T rightInclusive, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : IComparable<T> =>
+        BetweenHelper(value, leftInclusive, rightInclusive, paramName, true);
+
+    public static T IfNotBetween<T>(T value, T leftInclusive, T rightInclusive, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where T : IComparable<T> =>
+        BetweenHelper(value, leftInclusive, rightInclusive, paramName, false);
+
+    private static T BetweenHelper<T>(T value, T leftInclusive, T rightInclusive, string? paramName, bool isBetweenThrows)
+        where T : IComparable<T>
+    {
+        // The ArgumentOutOfRangeException.Throw* methods do NOT validate their parameters and allow
+        // NullReferenceExceptions to occur accordingly.
+
+        if (leftInclusive.CompareTo(rightInclusive) > 0)
+            (leftInclusive, rightInclusive) = (rightInclusive, leftInclusive);
+
+        bool isBetween = value.CompareTo(leftInclusive) >= 0 && value.CompareTo(rightInclusive) <= 0;
+
+        if (isBetween == isBetweenThrows)
+            BetweenHelperThrow(value, leftInclusive, rightInclusive, paramName, isBetweenThrows);
+
+        return value;
+    }
+
+    // Extracted in order to make the successful path smaller and more likely to inline.
+    [DoesNotReturn]
+    private static void BetweenHelperThrow<T>(T value, T leftInclusive, T rightInclusive, string? paramName, bool isBetweenThrows)
+        where T : IComparable<T> =>
+        throw new ArgumentOutOfRangeException(paramName, value,
+            string.Create(CultureInfo.InvariantCulture, $"{paramName} ('{value}') must {(isBetweenThrows ? "not " : string.Empty)}be between '{leftInclusive}' and '{rightInclusive}' inclusively."));
+
+    #endregion
+
+    #region Comparer<TEnum> Between (custom)
+
+    public static TEnum IfBetweenEnum<TEnum>(TEnum value, TEnum leftInclusive, TEnum rightInclusive, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where TEnum : struct, Enum =>
+        BetweenEnumHelper(value, leftInclusive, rightInclusive, paramName, true);
+
+    public static TEnum IfNotBetweenEnum<TEnum>(TEnum value, TEnum leftInclusive, TEnum rightInclusive, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+        where TEnum : struct, Enum =>
+        BetweenEnumHelper(value, leftInclusive, rightInclusive, paramName, false);
+
+    private static TEnum BetweenEnumHelper<TEnum>(TEnum value, TEnum leftInclusive, TEnum rightInclusive, string? paramName, bool isBetweenThrows)
+        where TEnum : struct, Enum
+    {
+        var comparer = Comparer<TEnum>.Default;
+
+        if (comparer.Compare(leftInclusive, rightInclusive) > 0)
+            (leftInclusive, rightInclusive) = (rightInclusive, leftInclusive);
+
+        bool isBetween = comparer.Compare(value, leftInclusive) >= 0 && comparer.Compare(value, rightInclusive) <= 0;
+
+        if (isBetween == isBetweenThrows)
+            BetweenEnumHelperThrow(value, leftInclusive, rightInclusive, paramName, isBetweenThrows);
+
+        return value;
+    }
+
+    // Extracted in order to make the successful path smaller and more likely to inline.
+    [DoesNotReturn]
+    private static void BetweenEnumHelperThrow<TEnum>(TEnum value, TEnum leftInclusive, TEnum rightInclusive, string? paramName, bool isBetweenThrows)
+        where TEnum : struct, Enum =>
+        throw new ArgumentOutOfRangeException(paramName, value,
+            string.Create(CultureInfo.InvariantCulture, $"{paramName} ('{value}') must {(isBetweenThrows ? "not " : string.Empty)}be between '{leftInclusive}' and '{rightInclusive}' inclusively."));
+
+    #endregion
+}
