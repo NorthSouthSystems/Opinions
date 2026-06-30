dc938b692d106ee3c05ea539fa46eb5e1e458f50
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2026-06-29 22:54:43 -0400

    Fixed a pair of Vogen analysis warnings.

--- NorthSouthSystems.Opinions.Entities.Vogen/Entities/TenantUser.cs
+++ NorthSouthSystems.Opinions.Entities.Vogen/Entities/TenantUser.cs
@@ -8,6 +8,8 @@ public readonly partial struct TenantUserId : IIntValueObject<TenantUserId>
     public static TenantUserId GlobalBootstrapper { get; } = new(1);
 
     private static Validation Validate(int id) => id > 0 ? Validation.Ok : Validation.Invalid();
+
+    private static int NormalizeInput(int input) => input; // No-op
 }
 
 [ValueObject<Guid>]
@@ -17,4 +19,6 @@ public readonly partial struct TenantUserIdForPublic : IGuidValueObject<TenantUs
         From(Guid.Parse("11111111-1111-1111-1111-111111111111", InvariantCulture));
 
     private static Validation Validate(Guid id) => id != Guid.Empty ? Validation.Ok : Validation.Invalid();
+
+    private static Guid NormalizeInput(Guid input) => input; // No-op
 }
