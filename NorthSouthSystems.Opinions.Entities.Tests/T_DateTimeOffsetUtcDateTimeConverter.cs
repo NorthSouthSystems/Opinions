370602df53e54a7e441584fd4e6055ef37849c61
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2026-09-19 15:33:12 -0400

    Created T_DateTimeOffsetUtcDateTimeConverter.

---
+++ NorthSouthSystems.Opinions.Entities.Tests/T_DateTimeOffsetUtcDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿public class T_DateTimeOffsetUtcDateTimeConverter
+{
+    [Fact]
+    public void Convert()
+    {
+        var converter = new DateTimeOffsetUtcDateTimeConverter();
+        var toDateTime = converter.ConvertToProviderExpression.Compile();
+        var fromDateTime = converter.ConvertFromProviderExpression.Compile();
+
+        var now = DateTimeOffset.UtcNow;
+
+        // Sanity
+        now.DateTime.Kind.Should().Be(DateTimeKind.Unspecified);
+        now.UtcDateTime.Kind.Should().Be(DateTimeKind.Utc);
+        now.LocalDateTime.Kind.Should().Be(DateTimeKind.Local);
+
+        toDateTime.Invoke(now).Should().Be(now.UtcDateTime);
+
+        fromDateTime.Invoke(now.DateTime).Should().Be(now);
+        fromDateTime.Invoke(now.UtcDateTime).Should().Be(now);
+
+        Action act = () => fromDateTime.Invoke(now.LocalDateTime);
+        act.Should().ThrowExactly<NotSupportedException>();
+    }
+}
