6af4813dd2c418aa130661a3a932a8f0f9694efa
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2026-09-22 07:48:25 -0400

    Added ProgramNorthSouthSystemsInfraAbstractions with helpers for wiring this new project into DI.

---
+++ NorthSouthSystems.Opinions.Infra.Abstractions/_ProgramNorthSouthSystemsInfraAbstractions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NorthSouthSystems.Infra;
+
+public static class ProgramNorthSouthSystemsInfraAbstractions
+{
+    public static IServiceCollection AddNorthSouthSystemsInfraAbstractionsDefaults(this IServiceCollection services)
+    {
+        Throw.IfNull(services).ConventionScanAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
+    }
+
+    public static Task InitNorthSouthSystemsInfraAbstractionsDefaultsAsync(this IServiceProvider serviceProvider) =>
+        Task.CompletedTask;
+}
